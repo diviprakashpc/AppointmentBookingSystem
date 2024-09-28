c04de3283d9f556cc62efdd8a81f35b503401554
BlackieStars <diviprakashpc@gmail.com>
2024-09-28 21:13:55 +0530

    Modify viewall apis done. Tested. All done

--- controllers/AppointmentControllers.js
+++ controllers/AppointmentControllers.js
@@ -1,7 +1,12 @@
 const Appointment = require("../models/AppointmentModel");
 const Doctor = require("../models/DoctorModel");
 const Patient = require("../models/PatientModel");
-const { CANCELLED, timeToMinutes, ValidateBody, BOOKED } = require("../utils/helper");
+const {
+  CANCELLED,
+  timeToMinutes,
+  ValidateBody,
+  BOOKED,
+} = require("../utils/helper");
 
 const CreateAppointmentController = async (request, response) => {
   try {
@@ -51,34 +56,44 @@ const CreateAppointmentController = async (request, response) => {
     const existingAppointment = await Appointment.findOne({
       patient: patient._id,
       status: { $ne: CANCELLED },
-      doctor : doctor._id
+      doctor: doctor._id,
     });
     if (existingAppointment) {
-      return response
-        .status(420)
-        .json({ error: "Patient already has an appointment booked with this doctor." });
+      return response.status(420).json({
+        error: "Patient already has an appointment booked with this doctor.",
+      });
     }
 
-    const formattedStartTime = timeToMinutes(startTime)
-    const formattedEndTime = timeToMinutes(endTime)
+    const formattedStartTime = timeToMinutes(startTime);
+    const formattedEndTime = timeToMinutes(endTime);
 
-    if(formattedEndTime <= formattedStartTime) {
+    if (formattedEndTime <= formattedStartTime) {
       return response.status(420).json({
-       error :  "End time cannot be greater than start time"
-      })
+        error: "End time cannot be greater than start time",
+      });
     }
 
-    if(formattedEndTime == 0){
+    if (formattedEndTime == 0) {
       return response.status(420).json({
-        "error" : "Invalid Request"
-      })
+        error: "Invalid Request",
+      });
     }
 
     const overlappingAppointment = await Appointment.findOne({
       doctor: doctor._id,
       $or: [
-        { 'timeStamp.startTime': { $lt: formattedEndTime, $gte: formattedStartTime } }, 
-        { 'timeStamp.endTime': { $gt: formattedStartTime, $lte: formattedEndTime } },
+        {
+          "timeStamp.startTime": {
+            $lt: formattedEndTime,
+            $gte: formattedStartTime,
+          },
+        },
+        {
+          "timeStamp.endTime": {
+            $gt: formattedStartTime,
+            $lte: formattedEndTime,
+          },
+        },
       ],
     });
 
@@ -98,8 +113,8 @@ const CreateAppointmentController = async (request, response) => {
         startTime: formattedStartTime,
         endTime: formattedEndTime,
       },
-      patientEmail : patient.email,
-      doctorEmail : doctor.email
+      patientEmail: patient.email,
+      doctorEmail: doctor.email,
     });
 
     const savedAppointment = await newAppointment.save();
@@ -115,18 +130,18 @@ const CreateAppointmentController = async (request, response) => {
   }
 };
 
-const CancelAppointmentController = async (request, response ) => {
+const CancelAppointmentController = async (request, response) => {
   try {
     const {
       patient: patientEmail,
-      doctor : doctorEmail,
+      doctor: doctorEmail,
       timeSlot: { startTime, endTime },
     } = request.body;
 
-    if(!ValidateBody([patientEmail, startTime, endTime])) {
+    if (!ValidateBody([patientEmail, startTime, endTime])) {
       return response.status(401).json({
-        "Error" : "Invalid Request"
-      })
+        Error: "Invalid Request",
+      });
     }
 
     const patient = await Patient.findOne({ email: patientEmail });
@@ -134,26 +149,28 @@ const CancelAppointmentController = async (request, response ) => {
       return response.status(460).json({ error: "Patient not found." });
     }
 
-    const doctor = await Doctor.findOne({email : doctorEmail})
+    const doctor = await Doctor.findOne({ email: doctorEmail });
 
     if (!doctor) {
       return response.status(460).json({ error: "Doctor not found." });
     }
 
-    const formattedStartTime = timeToMinutes(startTime)
-    const formattedEndTime = timeToMinutes(endTime)
-    console.log(startTime  + "   " +  endTime + "  " + formattedStartTime, + "         "+ formattedEndTime)
+    const formattedStartTime = timeToMinutes(startTime);
+    const formattedEndTime = timeToMinutes(endTime);
     // We will find appointment
     const appointment = await Appointment.findOne({
-      "patient": { $eq : patient._id},
-      "timeStamp.startTime":  { $eq : formattedStartTime},
-      "timeStamp.endTime": { $eq : formattedEndTime },
-      "doctor" : { $eq : doctor._id },
-      "status" : { $eq : BOOKED }
+      patient: { $eq: patient._id },
+      "timeStamp.startTime": { $eq: formattedStartTime },
+      "timeStamp.endTime": { $eq: formattedEndTime },
+      doctor: { $eq: doctor._id },
+      status: { $eq: BOOKED },
     });
 
     if (!appointment) {
-      return response.status(460).json({ error: "This Appointment is not found. It is either already cancelled or not present." });
+      return response.status(460).json({
+        error:
+          "This Appointment is not found. It is either already cancelled or not present.",
+      });
     }
 
     appointment.status = CANCELLED;
@@ -170,4 +187,146 @@ const CancelAppointmentController = async (request, response ) => {
   }
 };
 
-module.exports = { CancelAppointmentController, CreateAppointmentController };
+const ViewAllAppointmentsByDoctor = async (req, res) => {
+  const { doctorEmail } = req.params;
+  const params = [];
+
+  if (!ValidateBody(Object.values(req.params))) {
+    return res.status(421).json({
+      msg: "Invalid data to create",
+    });
+  }
+
+  const doctor = await Doctor.findOne({
+    email: { $eq: doctorEmail },
+  });
+
+  if (!doctor) {
+    return res.status(420).json({ error: "Doctor not found." });
+  }
+
+  const appointments = await Appointment.find({
+    doctor: { $eq: doctor._id },
+  });
+  // If I dont use await I get query returned that I can run to get the actual data.
+  // I can run using appointments.exec()
+
+  return res.status(200).json({
+    code: "200",
+    model: appointments,
+    msg: "Success",
+  });
+};
+
+const GetAllAppointmentsByUser = async (req, res) => {
+  if (!ValidateBody(Object.values(req.query))) {
+    return res.status(421).json({
+      msg: "Invalid data to create",
+    });
+  }
+
+  const { patientEmail } = req.query;
+  const params = [];
+
+  const patient = await Patient.findOne({
+    email: { $eq: patientEmail },
+  });
+
+  if (!patient) {
+    return res.status(420).json({ error: "Patient not found." });
+  }
+
+  const appointments = await Appointment.find({
+    patient: { $eq: patient._id },
+  });
+
+  return res.status(200).json({
+    code: "200",
+    model: appointments,
+    msg: "Success",
+  });
+};
+
+const ModifyAppointmentDetails = async (req, res) => {
+  try {
+    const { patientEmail, doctorEmail, newTimeSlot } = req.body;
+    console.log(req.body)
+    if (
+      !patientEmail ||
+      !doctorEmail ||
+      !newTimeSlot ||
+      !newTimeSlot.startTime ||
+      !newTimeSlot.endTime
+    ) {
+      return res.status(400).json({ error: "Incomplete request data." });
+    }
+    const patient = await Patient.findOne({ email: patientEmail });
+    if (!patient) {
+      return res.status(404).json({ error: "Patient not found." });
+    }
+    const doctor = await Doctor.findOne({ email: doctorEmail });
+    if (!doctor) {
+      return res.status(404).json({ error: "Doctor not found." });
+    }
+    const appointment = await Appointment.findOne({
+      patient: patient._id,
+      doctor: doctor._id,
+      status : { $ne : CANCELLED}
+    });
+    if (!appointment) {
+      return res.status(404).json({ error: "Appointment not found or Appointment is cancelled" });
+    }
+    
+    const formattedNewStartTime = timeToMinutes(newTimeSlot.startTime);
+    const formattedNewEndTime = timeToMinutes(newTimeSlot.endTime);
+    if (formattedNewEndTime <= formattedNewStartTime) {
+      return res.status(420).json({
+        error: "End time cannot be greater than start time",
+      });
+    }
+    if (formattedNewEndTime == 0) {
+      return res.status(420).json({
+        error: "Invalid Request",
+      });
+    }
+    const overlappingAppointment = await Appointment.findOne({
+      _id: { $ne: appointment._id },
+      //  We want to not check the appointment that's currently being modified (appointment._id)
+      $or: [
+        {
+          "timeStamp.startTime": {
+            $lt: formattedNewEndTime,
+            $gte: formattedNewStartTime,
+          },
+        },
+        {
+          "timeStamp.endTime": {
+            $gt: formattedNewStartTime,
+            $lte: formattedNewEndTime,
+          },
+        },
+      ],
+    });
+    if (overlappingAppointment) {
+      return res.status(409).json({ error: "Time slot is already booked." });
+    }
+    appointment.timeStamp.startTime = formattedNewStartTime;
+    appointment.timeStamp.endTime = formattedNewEndTime;
+    const savedAppointment = await appointment.save();
+    return res.status(200).json({
+      message: "Appointment modified successfully.",
+      appointment: savedAppointment,
+    });
+  } catch (error) {
+    console.error("Error modifying appointment:", error);
+    return res.status(500).json({ error: "Failed to modify appointment." });
+  }
+};
+
+module.exports = {
+  CancelAppointmentController,
+  CreateAppointmentController,
+  ViewAllAppointmentsByDoctor,
+  GetAllAppointmentsByUser,
+  ModifyAppointmentDetails,
+};
--- routes/appointmentRoutes.js
+++ routes/appointmentRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { CancelAppointmentController, CreateAppointmentController } = require("../controllers/AppointmentControllers");
+const { CancelAppointmentController, CreateAppointmentController, ViewAllAppointmentsByDoctor, GetAllAppointmentsByUser, ModifyAppointmentDetails } = require("../controllers/AppointmentControllers");
 const router = express.Router();
 
 
@@ -7,6 +7,14 @@ router.route("/create").post(CreateAppointmentController)
 
 router.route("/cancel").post(CancelAppointmentController)
 
+router.route("/viewAll/:doctorEmail").get(ViewAllAppointmentsByDoctor) // One way to pass params.
+
+router.route("/getAll").get(GetAllAppointmentsByUser) // This way i pass query and url and take it.
+
+router.route("/modify").post(ModifyAppointmentDetails)
+
+
+// In each controller we are validating the existence of patient and doctor. We can create a middleware for that. 
 
 
 module.exports = router
